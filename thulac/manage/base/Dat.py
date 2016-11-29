eecec3bb53c9989370fcf61ea33ec9ed2e504b06
sooda <liushouda@qq.com>
2016-11-29 19:35:54 +0800

    crack dictionary hash data
    对hash加密的字典进行解密。

--- thulac/manage/base/Dat.py
+++ thulac/manage/base/Dat.py
@@ -1,4 +1,5 @@
-#coding = utf-8
+#! /usr/bin/env python2.7
+#coding=utf-8
 import struct
 import os
 
@@ -9,11 +10,62 @@ class Dat:
         inputfile = open(filename, "rb")
         
         self.datSize = os.path.getsize(filename) / 8
+        print self.datSize
         s = inputfile.read(8 * self.datSize)
         tmp = "<"+str(self.datSize*2)+"i"
         self.dat = struct.unpack(tmp, s)
         self.dat = tuple(self.dat)
         inputfile.close()
+        self.crack_name = os.path.join("thulac/crack_models", os.path.basename(filename))
+        #self.crack()
+        #self.crack_with_prefix()
+
+
+    def do_crack(self, depth, base, data_size, word):
+        depth = depth + 1
+        ind = base * 2
+        if base > data_size or depth > 8:
+            return word
+        else:
+            for j in xrange(1, data_size):
+                index = self.dat[ind] + j
+                if index < data_size and self.dat[2 * index + 1] == base:
+                    #print index, base, unichr(j), j
+                    word = word + unichr(j)
+                    base = index
+                    return self.do_crack(depth, base, data_size, word)
+                else:
+                    continue
+            return word
+
+    def crack(self):
+        self.crack_fid = open(self.crack_name, 'w')
+        data_size = self.datSize
+        for i in xrange(1, data_size):
+            if self.dat[i*2+1] == 0: #begin word
+                word = unichr(i)
+                base = i
+                result = self.do_crack(1, base, data_size, word)
+                #print result
+                self.crack_fid.write(result+"\n")
+        self.crack_fid.close()
+
+    def crack_with_prefix(self):
+        prefix = "旧事重".decode('utf8')
+        ind = 0
+        base = 0
+        for i in range(len(prefix)):
+            ind = self.dat[2 * ind] + ord(prefix[i])
+            base = ind
+            print ind
+            if ind > self.datSize and self.dat[2 * ind + 1] == base:
+                break
+        ind = ind * 2
+
+
+        result = self.do_crack(2, base, self.datSize, prefix)
+        print result
+
 
     
     def printDat(self, filename):
@@ -72,7 +124,9 @@ class Dat:
         ind = 0
         base = 0
         for i in range(len(prefix)):
+            data = self.dat[2*ind]
             ind = self.dat[2 * ind] + ord(prefix[i])
+            print prefix[i], ord(prefix[i]), data, ind, self.dat[2*ind+1], base
             if((ind >= self.datSize) or self.dat[2 * ind + 1] != base):
                 return i
             base = ind
@@ -83,8 +137,9 @@ class Dat:
     
     def getDat(self):
         return self.dat
-    
+
 if __name__ == '__main__':
-    dat = Dat("cws_dat.bin")
-    
+    dat = Dat("thulac/models/idiom.dat")
+    #dat = Dat("thulac/models/ns.dat")
+
         
